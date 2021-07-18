8b7e1cd41f51d2ff886c2e01d17751807759aec5
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-07-19 03:05:11 +0530

    bind todo state value to form input

--- src/components/TodoInput/index.js
+++ src/components/TodoInput/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoInputContainer, TodoInputTextBox } from './TodoInput.elements';
 import { StyledCheckBox } from '../shared';
 
 const TodoForm = () => {
+
+    const [todoValue, setTodoValue] = useState('');
+
+    const onChange = e => setTodoValue(e.target.value);
+
+    const addTodo = (e) => {
+        e.preventDefault();
+        if(!todoValue.trim()) {
+            return alert('Please write todo item before submitting')
+        }
+
+        console.log(todoValue);
+    }
+
     return (
-        <TodoInputContainer>
+        <TodoInputContainer onSubmit={addTodo}>
             <StyledCheckBox disabled checked={false}/>
-            <TodoInputTextBox placeholder="Create a new todo..."/>
+            <TodoInputTextBox placeholder="Create a new todo..." value={todoValue} onChange={onChange}/>
         </TodoInputContainer>
     );
 }
